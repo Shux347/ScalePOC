0db216d0e66b89b9725a9fec7d553783541b1395
Varun Shukla <varun.shukla02@sap.com>
2025-06-20 05:41:15 +0000

    Correct the colour coding

--- srv/cat-service.js
+++ srv/cat-service.js
@@ -8,13 +8,16 @@ module.exports = cds.service.impl(function() {
 
         if (daysUntilCheck < 0) {
             each.status = 'Urgent';
-            each.status_criticality = 3; // Red
+            // Value 1 corresponds to Red in this UI5 version
+            each.status_criticality = 1;
         } else if (daysUntilCheck <= 30) {
             each.status = 'Warning';
-            each.status_criticality = 2; // Yellow
+            // Value 2 corresponds to Yellow in this UI5 version
+            each.status_criticality = 2;
         } else {
             each.status = 'OK';
-            each.status_criticality = 1; // Green
+            // Value 3 corresponds to Green in this UI5 version
+            each.status_criticality = 3;
         }
     });
 });
